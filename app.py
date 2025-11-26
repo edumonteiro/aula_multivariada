669e17ed9d333c7a3168f38e2f3efd0b323e04f8
Eduardo <edunb01@gmail.com>
2025-11-26 03:23:42 -0300

    fixed_bug

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def plot_sklearn_final(X_2d, pca, kmeans_sklearn):
         s=60,
         alpha=0.8,
         ax=ax,
-        legend=True
+        legend=True,
         style=df_importado$am
     )
 
@@ -406,6 +406,7 @@ plt.show()"""
 
 if __name__ == "__main__":
     main()
+
 
 
 
